6e68739bab9fb2c1db130d9464f17c33f3bfcc22
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-04-03 16:43:30 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
    
    inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    catppuccin.url = "github:catppuccin/nix";
+    catppuccin.url = "github:catppuccin/nix"; 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
