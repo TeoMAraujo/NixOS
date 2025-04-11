33fe61759c3af17027da53039ab32aaad8cc241d
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-04-11 15:32:11 -0300

    a
    a

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   description = "Nixos config flake";
-
-
    inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     catppuccin.url = "github:catppuccin/nix";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -14,7 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     }; 
     flake-parts.url = "github:hercules-ci/flake-parts";
-  };
+};    
   outputs = { self, nixpkgs, home-manager, catppuccin, ... }@inputs:
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
@@ -34,6 +31,7 @@
           ./default/configuration.nix
           catppuccin.nixosModules.catppuccin
           inputs.home-manager.nixosModules.default
+          inputs.nixvim.nixosModules.nixvim
         ];
       };
     };
