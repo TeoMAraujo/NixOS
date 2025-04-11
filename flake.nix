4744db0ed0c4bd458f5ab413ea5b4afaaaa4fab2
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-04-10 23:24:09 -0300

    a
    a

--- flake.nix
+++ flake.nix
@@ -1,21 +1,22 @@
 {
   description = "Nixos config flake";
-   
-   
+
+
    inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    catppuccin.url = "github:catppuccin/nix"; 
-    nvf.url = "github:notashelf/nvf";
+    catppuccin.url = "github:catppuccin/nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
- #   nixvim = {
-  #    url = "github:TeoMAraujo/NixVim";
-  #    inputs.nixpkgs.follows = "nixpkgs";
-#    };# If using a stable channel you can use `url = "github:nix-community/nixvim/nixos<version>"`
+    nixvim = {
+      url = "github:TeoMAraujo/NixVim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };# If using a stable channel you can use `url = "github:nix-community/nixvim/nixos<version>"`
   };
-  outputs = { self, nixpkgs, home-manager, catppuccin, nvf, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, catppuccin, ... }@inputs:
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
     let
 		system = "x86_64-linux";
 		pkgs = nixpkgs.legacyPackages.${system};
@@ -26,21 +27,12 @@
           inherit pkgs;
           modules = [ ./default/home.nix ];
 	};
-	packages.x86_64-linux. default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [./nvf-configuration.nix];
-        }).neovim;
-
-	legacyPackages
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
         modules = [
           ./default/configuration.nix
           catppuccin.nixosModules.catppuccin
           inputs.home-manager.nixosModules.default
-          nvf.nixosModules.default
-
         ];
       };
     };
