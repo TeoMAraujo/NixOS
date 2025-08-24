de476980bd50165aad818555aaf6fd28171457e9
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-08-24 13:43:04 -0300

    Update home.nix

--- default/home.nix
+++ default/home.nix
@@ -1,44 +1,50 @@
- { config, pkgs, inputs, nixvim,... }:
-  
-   {
-     home.username = "paula";
-     home.homeDirectory = "/home/paula";
-  
-     home.stateVersion = "24.11"; # Please read the comment before changing.
-        home.packages = with pkgs; [ 
-         #inputs.nixvim.packages.${pkgs.system}.default 
+{
+  config,
+  pkgs,
+  inputs,
+  nixvim,
+  ...
+}:
+
+{
+  home.username = "paula";
+  home.homeDirectory = "/home/paula";
+
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.packages = with pkgs; [
+    #inputs.nixvim.packages.${pkgs.system}.default
+  ];
+
+  programs = {
+    ghostty = {
+      enable = true;
+      settings = {
+        theme = "catppuccin-mocha";
+        font-size = 10;
+        background-opacity = 0.8;
+        cursor-style = "bar";
+        cursor-style-blink = true;
+        adjust-cursor-thickness = 2;
+        keybind = [
+          "ctrl+h=goto_split:left"
+          "ctrl+l=goto_split:right"
         ];
- 
-   programs = {
-        ghostty = {
-        enable = true;
-          settings = {
-            theme = "catppuccin-mocha";
-            font-size = 10;
-            background-opacity = 0.8;
-            cursor-style = "bar";
-            cursor-style-blink = true;
-            adjust-cursor-thickness = 2;
-            keybind = [
-                "ctrl+h=goto_split:left"
-                "ctrl+l=goto_split:right"
-            ];
-          };
-        };
- 
-        git = {
-          enable = true;
-          userName = "TeoMAraujo";
-          userEmail = "teoaraujoufmg@gmail.com";
-        };
+      };
+    };
+
+    git = {
+      enable = true;
+      userName = "TeoMAraujo";
+      userEmail = "teoaraujoufmg@gmail.com";
+    };
   };
-       home.file = {
-    };
- 
-    home.sessionVariables = {
-      # EDITOR = "emacs";
-    };
- 
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
-  }
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
