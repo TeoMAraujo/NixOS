734faeba8e05894870c97f1099ae0a91af1ae3c4
TeoMAraujo <teoaraujoufmg@gmail.com>
2026-08-21 02:53:06 -0300

    a

---
+++ flatpak.nix
@@ -0,0 +1,31 @@
+{ lib, self, ... }:
+{
+    services.flatpak = {
+        enable = true;
+        
+        update.onActivation = true;
+
+        remotes = lib.mkOptionDefault [
+            {
+                name = "flathub";
+                location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+            }
+        ];
+
+        packages = [
+            { appId = "net.waterfox.waterfox"; origin = "flathub"; }
+            { appId = "com.stremio.Stremio"; origin = "flathub"; }
+            { appId = "md.obsidian.Obsidian"; origin = "flathub"; }
+        ];
+
+        overrides.global.Environment.TZ = ":/run/host/monitor/localtime";
+        overrides.writeMode = "merge";
+        
+        uninstallUnmanaged = true;
+    };
+
+    programs.appimage = {
+        enable = true;
+        binfmt = true;
+    };
+}
